c7ab45cd1b702ded18fd3049b7775fb63b66b2d9
Daniel Poetzl <daniel.poetzl@cs.ox.ac.uk>
2016-11-07 17:50:38 +0000

    unwind bounds regression tests

---
+++ regression/unwind-bounds/unwind_26/main.c
@@ -0,0 +1,11 @@
+
+int f00 (int x) {
+	do {
+		int y;
+		y = x + x;
+		x = y;
+	} while (0);
+
+	return x;
+}
+
---
+++ regression/unwind-bounds/unwind_32/20160804_unwind_2.c
@@ -0,0 +1,10 @@
+int g_a[4];
+#define SZ(a)   sizeof(a)/sizeof(a[0])
+const int g_a_SZ = SZ(g_a);
+
+void reset(void)
+{
+  int i;
+  for (i=0; i < g_a_SZ; i++)    //  4 times
+    g_a[i] = 0;
+}
