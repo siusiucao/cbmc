a3dab626ad2a22edb43cef0f9a01557899a10d2c
thk123 <thomas.kiley@diffblue.com>
2017-02-28 19:18:33 +0000

    Added flags so the factory reads the flags correctly

--- src/goto-analyzer/goto_analyzer_parse_options.h
+++ src/goto-analyzer/goto_analyzer_parse_options.h
@@ -141,6 +141,7 @@ class optionst;
   "(non-null)(show-non-null)" \
   "(variable)" \
   "(variable-sensitivity)" \
+  "(pointers)(arrays)(structs)" \
   "(constants)" \
   "(dependence-graph)" \
   "(show)(verify)(simplify):" \
