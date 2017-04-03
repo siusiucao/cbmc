993d05e95aa0da0a55aff391aa6118fd9a32b185
thk123 <thomas@diffblue.com>
2017-04-03 15:56:40 +0100

    Moved the function call handling into a separate method for readability

--- src/analyses/variable-sensitivity/variable_sensitivity_domain.h
+++ src/analyses/variable-sensitivity/variable_sensitivity_domain.h
@@ -107,6 +107,14 @@ public:
   bool is_top() const override;
 
 private:
+  void transform_function_call(
+    locationt from,
+    locationt to,
+    ai_baset &ai,
+    const namespacet &ns);
+
+  bool ignore_function_call_transform(const irep_idt &function_id) const;
+
   abstract_environmentt abstract_state;
 };
 
