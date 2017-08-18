856e7e7ae4f00da04fcc4d1dcfc057ba6b7d45ca
thk123 <thomas@diffblue.com>
2017-08-18 09:44:34 +0100

    Another temp checkin of where we've got to

--- src/analyses/ai.h
+++ src/analyses/ai.h
@@ -361,6 +361,8 @@ protected:
   // abstract methods
 
   virtual bool merge(const statet &src, locationt from, locationt to)=0;
+
+  virtual std::vector<symbol_exprt> get_modified_symbols(const statet &src, const statet &other)=0;
   // for concurrent fixedpoint
   virtual bool merge_shared(
     const statet &src,
@@ -453,6 +455,12 @@ protected:
     statet &dest=get_state(to);
     return static_cast<domainT &>(dest).merge(
       static_cast<const domainT &>(src), from, to);
+  }
+
+  std::vector<symbol_exprt> get_modified_symbols(const statet &src, const statet &other) override
+  {
+    return static_cast<const domainT &>(src).get_modified_symbols(
+      static_cast<const domainT &>(other));
   }
 
   std::unique_ptr<statet> make_temporary_state(const statet &s) override
